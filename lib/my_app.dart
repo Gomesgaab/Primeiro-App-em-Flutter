179ce1c1f7b431b6acf5f296e31ba21082374a0f
Gomesgaab <gabrielgabiga870@gmail.com>
2023-12-20 13:31:59 -0300

    Oitavo Commit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:srh/pages/Main_page.dart';
 import 'package:srh/pages/login_page.dart';
 
 class MyApp extends StatelessWidget {
